81f0412349f9e88dc0e39620034a9b80aee7f319
Stephen <stephenrs7@gmail.com>
2019-08-30 16:07:03 -0700

    submission

--- app.js
+++ app.js
@@ -2,16 +2,44 @@ var images = {
 	man: {
 		heading: 'Man Image',
 		description: 'Image of a man.',
-		path: 'img/man.jpg'
+		path: 'images/man.jpg'
 	},
 	wizard: {
 		heading: 'Wizard Image',
 		description: 'Image of a wizard.',
-		path: 'img/wizard.jpg'
+		path: 'images/wizard.jpg'
 	},
 	beast: {
 		heading: 'Beast Image',
 		description: 'Image of a beast.',
-		path: 'img/beast.jpg'
+		path: 'images/beast.jpg'
 	}
 };
+
+let imageArray = [images.man, images.wizard, images.beast];
+
+window.onload = () => {
+
+	for (let image of imageArray) {
+		document.getElementById('image-section').innerHTML += `
+			<div class = 'image-column' onmouseover='showText(this)' onmouseout='removeText(this)'>
+				<img src = ${image.path} class = 'image' style = 'width:100%' >
+				<div class='image-text'>
+					<h3>${image.heading}<h3>
+					<h4>${image.description}<h4>
+				<div>
+			</div>
+		`
+	}
+}
+	{
+	let showText = (element, x) => {
+		let div = element.querySelector('.image-column')
+		div.style.visibility = 'visible';
+	}
+	
+	let removeText = (element) => {
+		let div = element.querySelector('.image-column')
+		div.style.visibility = 'hidden';
+	}
+}
